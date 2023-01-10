9cf8d5da2d8322a8632666169293ea56e7b4fdbc
sting-byte <60401828+sting-byte@users.noreply.github.com>
2023-01-10 17:30:05 +0100

    Add files via upload

---
+++ site/index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_d9co79q";
+    const templateID = "template_ran71mo";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
